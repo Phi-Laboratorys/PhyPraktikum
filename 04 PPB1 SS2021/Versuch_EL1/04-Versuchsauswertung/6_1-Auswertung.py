e67fcd3030d15e18b5e38f5f0721485319b057a0
Manuel Lippert <73902879+ManeLippert@users.noreply.github.com>
2021-05-25 22:43:04 +0200

    Abschluss PPB1

---
+++ 04 PPB1 SS2021/Versuch_EL1/04-Versuchsauswertung/6_1-Auswertung.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline, BSpline
+from scipy.optimize import curve_fit
+
+data = 'Versuch_EL1/04-Versuchsauswertung/6_1-Messwerte.csv'
+
+# Select Data1
+df1 = pd.read_csv(data)
+df1.index = np.arange(1, len(df1)+1)
+df1 = df1.round({'s_(I_B)':2, 's_(U_BE)':3})
+
+'''
+# Plot Data1
+x, x_err = df1['U_BE'], df1['s_(U_BE)']
+y, y_err = df1['I_B'], df1['s_(I_B)']
+
+# Interpolation
+x_new = np.linspace(x.min(), x.max(), 300)
+spl = make_interp_spline(x, y, k=3)  # type: BSpline
+y_smooth = spl(x_new)
+
+fig = plt.figure(figsize=(15,10))
+ax = fig.add_subplot(111)
+ax.errorbar(x,y,yerr=y_err,fmt='o',capsize=5,label='Messreihe')
+ax.plot(x_new, y_smooth, color='r', label='Spline-Interpolation 3. Grades')
+#ax.set_title(r'$$-$\omega_*$-Diagramm')
+ax.set_ylabel(r'$I_B$ in mA')
+ax.set_xlabel(r'$U_{BE}$ in V')
+ax.legend()
+plt.show()
+'''
+
+# Select Data2
+x, x_err = df1['I_B'], df1['s_(I_B)']
+y, y_err = df1['U_BE'], df1['s_(U_BE)']
+
+df2 = pd.DataFrame() 
+
+# Fehler
+i = 1
+rBE, s_rBE = [], []
+di, s_di = [], []
+du, s_du = [], []
+mean_i = []
+
+while i < len(x):
+    rBE.append((y[i+1]-y[i])/(x[i+1]-x[i]))
+    di.append(x[i+1]-x[i])
+    du.append(y[i+1]-y[i])
+    mean_i.append((x[i+1]+x[i])/2)
+    s_di.append(np.sqrt(x_err[i+1]**2+x_err[i]**2))
+    s_du.append(np.sqrt(y_err[i+1]**2+y_err[i]**2))
+    i += 1   
+
+df2['d_(I_B)'] = di
+df2['s_(d_(I_B))'] = s_di
+df2['d_(U_BE)'] = du
+df2['s_(d_(U_BE))'] = s_du
+df2['r_BE'] = rBE
+df2['s_(r_BE)'] = np.sqrt((df2['s_(d_(U_BE))']/df2['d_(I_B)'])**2 + 
+                          ((df2['d_(U_BE)']/df2['d_(I_B)'])*(df2['s_(d_(I_B))']/df2['d_(I_B)']))**2)
+df2['m_(I_B)'] = mean_i
+
+df2.index = np.arange(1, len(df2)+1)
+df1 = df1.drop([39]) 
+df2['I_B'] = df1['I_B']
+df2 = df2.round({'s_(d_(I_B))':2, 's_(d_(U_BE))':3, 'r_BE':3, 's_(r_BE)':2})
+print(df2)
+
+'''
+# Plot Data2
+# x, x_err = df1['I_B'], df1['s_(I_B)']
+x, x_err = df2['m_(I_B)'], df2['s_(d_(I_B))']
+y, y_err = df2['r_BE'], df2['s_(r_BE)']
+
+# Excluding oversized Data
+y_err[0:4] = 0
+
+# Curve Fit
+def objective(x, a, b):
+	return a/x + b
+
+popt, _ = curve_fit(objective, x, y)
+a, b = popt
+x_line = np.linspace(x.min(), x.max(), 300)
+y_line = objective(x_line, a, b)
+print(a, b)
+
+fig = plt.figure(figsize=(15,10))
+ax = fig.add_subplot(111)
+ax.errorbar(x,y,yerr=y_err,fmt='o',capsize=3,label='Messreihe')
+ax.plot(x_line, y_line, color='r', 
+        label='Curve-Fit\n' + r'$f(x)=\frac{a}{x}+b$' + '\n' +'a = 0.01891727051649630\nb = 0.02821377849572285')
+#ax.set_title(r'$$-$\omega_*$-Diagramm')
+ax.set_xlabel(r'$I_B$ in mA')
+ax.set_ylabel(r'$r_{BE}$ in k$\Omega$')
+ax.set_ylim(-0.2,2)
+ax.legend()
+plt.show()
+'''
---
+++ 04 PPB1 SS2021/Versuch_EL2/20-Plots.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+anzahl = 300
+x = np.linspace(1/10, 10, anzahl)
+#y0 = np.repeat(1/10, anzahl)
+
+'''INTEGRATIONSSCHALTUNG'''
+
+yi1 = 1/x
+yi2 = 1/np.sqrt(1+x**2)
+
+x_igrh = np.linspace(1/10, 1, anzahl)
+y_igrh = np.repeat(1/np.sqrt(2), anzahl)
+x_igrv = np.repeat(1, anzahl)
+y_igrv = np.linspace(1/10, 1/np.sqrt(2), anzahl)
+
+#plt.plot(x, y0)
+
+plt.plot(x_igrh, y_igrh, color='r', linestyle='--', label=r'$\omega_\mathregular{i,gr}$')
+plt.plot(x_igrv, y_igrv, color='r', linestyle='--')
+plt.plot(x, yi1, label=r'$\tilde{v}_\mathregular{i,1}$')
+plt.plot(x, yi2, label=r'$\tilde{v}_\mathregular{i,2}$')
+plt.xscale('log')
+plt.xlabel(r'$\tilde{\omega}_\mathregular{i}$')
+plt.yscale('log')
+plt.ylabel(r'$\tilde{v}$')
+plt.legend()
+plt.show()
+
+'''DIFFERENZIERSCHALTUNG'''
+
+yd1 = x
+yd2 = x/np.sqrt(1+x**2)
+
+x_dgrh = np.linspace(1, 10, anzahl)
+y_dgrh = np.repeat(1/np.sqrt(2), anzahl)
+x_dgrv = np.repeat(1, anzahl)
+y_dgrv = np.linspace(1/10, 1/np.sqrt(2), anzahl)
+
+#plt.plot(x, y0)
+
+plt.plot(x_dgrh, y_dgrh, color='r', linestyle='--', label=r'$\omega_\mathregular{d,gr}$')
+plt.plot(x_dgrv, y_dgrv, color='r', linestyle='--')
+plt.plot(x, yd1, label=r'$\tilde{v}_\mathregular{d,1}$')
+plt.plot(x, yd2, label=r'$\tilde{v}_\mathregular{d,2}$')
+plt.xscale('log')
+plt.xlabel(r'$\tilde{\omega}_\mathregular{d}$')
+plt.yscale('log')
+plt.ylabel(r'$\tilde{v}$')
+plt.legend()
+plt.show()
+
+'''KOMBINATIONSSCHALTUNG'''
+p = 1000
+R1, R2 = p, 1/p
+C1, C2 = p, 1/p
+wi, wd, w0 = 1/(R2*C2), 1/(R1*C1), 1
+ywi, ywd, yw0 = 1/np.sqrt((R1/R2+C2/C1)**2+(wi*R1*C2-1/(wi*R2*C1))**2), 1/np.sqrt((R1/R2+C2/C1)**2+(wd*R1*C2-1/(wd*R2*C1))**2), 1/np.sqrt((R1/R2+C2/C1)**2+(w0*R1*C2-1/(w0*R2*C1))**2)
+yk = 1/np.sqrt((R1/R2+C2/C1)**2+(x*R1*C2-1/(x*R2*C1))**2)
+
+w_i, y_wi = np.repeat(wi, anzahl), np.linspace(1/10, ywi, anzahl)
+w_d, y_wd = np.repeat(wd, anzahl), np.linspace(1/10, ywd, anzahl)
+w_0, y_w0 = np.sqrt(w_i*w_d), np.linspace(1/10, yw0, anzahl)
+
+#plt.plot(x, yi1, Label=r'$\tilde{v}_\mathregular{i,1}$', color='grey')
+#plt.plot(x, yi2, Label=r'$\tilde{v}_\mathregular{i,2}$', color='grey')
+#plt.plot(x, yd1, Label=r'$\tilde{v}_\mathregular{d,1}$', color='grey')
+#plt.plot(x, yd2, Label=r'$\tilde{v}_\mathregular{d,2}$', color='grey')
+plt.plot(x,yk)
+#plt.plot(w_d,y_wd, linestyle='--', label= r'$\omega_\mathregular{d,gr}$')
+#plt.plot(w_i,y_wi, linestyle='--', label= r'$\omega_\mathregular{i,gr}$')
+#plt.plot(w_0,y_w0, linestyle='--', label= r'$\omega_{0}$')
+plt.xscale('log')
+plt.xlabel(r'$\omega$ in $\frac{1}{\mathregular{s}}$')
+plt.yscale('log')
+plt.ylabel(r'$v_\mathregular{d,i}$')
+plt.legend()
+plt.show()
