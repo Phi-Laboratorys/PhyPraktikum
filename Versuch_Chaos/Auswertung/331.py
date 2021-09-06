cc66f701829be06b1afc21f77e39503880918e28
ManeLippert <manuel-lippert@outlook.de>
2021-09-06 20:00:48 +0200

    Chaos: Versuchstag

--- Versuch_Chaos/Auswertung/331.py
+++ Versuch_Chaos/Auswertung/331.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
 
-data = "Versuch_Chaos/Auswertung/Bifurkation1.csv"
+data = "Versuch_Chaos/Auswertung/Bifurkation.csv"
 df = pd.read_csv(data)
 
 #Berechnung
 df = df.sort_values(by='Masse [g]')
 df["Delta^2"] = (df["UAl [V]"] - df["UAr [V]"])**2
-print(df)
+print(df.to_latex())
 
 #Fehlerrechnung
-
+'''
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
@@ -19,8 +19,4 @@ ax2 = fig.add_subplot(1, 2, 2)
 ax1.errorbar(df["Masse [g]"][7:],df["Delta^2"][7:], yerr=df["sUA"][7:], fmt='x', capsize=5)
 ax2.plot(df["Masse [g]"],df["UAl [V]"], 'o')
 ax2.plot(df["Masse [g]"],df["UAr [V]"], 'o')
-plt.show()
-
-
-
-
+plt.show()'''
