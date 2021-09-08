4226a14a382e1fb87735fca9883092e8a4010487
ManeLippert <manuel-lippert@outlook.de>
2021-09-08 18:28:09 +0200

    Chaos: Auswertung Teil 1

--- Versuch_Chaos/41-Bifurkation.py
+++ Versuch_Chaos/41-Bifurkation.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
+from matplotlib import rc
+
+rc('text', usetex=True)
+rc('font', family='serif')
 
 data = "Versuch_Chaos/Daten/Pendel/3.1/BifurkationPendel.csv"
 df = pd.read_csv(data)
@@ -8,15 +12,10 @@ df = pd.read_csv(data)
 #Berechnung
 df = df.sort_values(by='Masse [g]')
 df["Delta^2"] = (df["UAl [V]"] - df["UAr [V]"])**2
-print(type(df['UAl [V]'][0]))
 
-#Fehlerrechnung
-'''
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(1, 2, 2)
-
-ax1.errorbar(df["Masse [g]"][7:],df["Delta^2"][7:], yerr=df["sUA"][7:], fmt='x', capsize=5)
-ax2.plot(df["Masse [g]"],df["UAl [V]"], 'o')
-ax2.plot(df["Masse [g]"],df["UAr [V]"], 'o')
-plt.show()'''
+plt.plot(df["Masse [g]"],df["UAl [V]"], 'o', color='orange')
+plt.plot(df["Masse [g]"],df["UAr [V]"], 'o', color='orange')
+plt.xlabel(r'$M$ in g')
+plt.ylabel(r'$U_a$ in V')
+#plt.errorbar(df["Masse [g]"][7:],df["Delta^2"][7:], yerr=df["sUA"][7:], fmt='x', capsize=5)
+plt.show()
