73f9a353db51bf02a1be4d0a6076ace24187b9a2
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-09-08 18:59:46 +0200

    PPB2 Chaos; PythonSkr. AUfg4b

--- Versuch_Chaos/42-TeilauswertungPaul/42-2-AufgB.py
+++ Versuch_Chaos/42-TeilauswertungPaul/42-2-AufgB.py
@@ -14,22 +14,29 @@ data45 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=45kO/06_09_2021_19_31_20_G11_sh
 data60 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=60kO/06_09_2021_19_32_51_G11_shinriki_0.dat'
 data66 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=66kO/06_09_2021_19_34_38_G11_shinriki_0.dat'
 data70 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=70kO/06_09_2021_19_36_25_G11_shinriki_0.dat'
+data73 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=73kO/06_09_2021_19_37_54_G11_shinriki_0.dat'
+data74 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=74kO/06_09_2021_19_39_09_G11_shinriki_0.dat'
+data87 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=87kO/06_09_2021_19_41_14_G11_shinriki_0.dat'
+data91 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=91kO/06_09_2021_19_42_12_G11_shinriki_0.dat'
+data100 = 'Versuch_Chaos/Daten/Shinriki/Aufg-b/R1=100kO/06_09_2021_19_43_06_G11_shinriki_0.dat'
+
+data = data70
 df = pd.read_csv(data, delim_whitespace=True, skiprows=7, decimal=',')
 
 #print(df.head())
 
-x=df['V1(V)'][0:100]
-y=df['V2(V)'][0:100]
-z=df['V3(V)'][0:100]
+x=df['V1(V)']#[0:1000]
+y=df['V2(V)']#[0:1000]
+z=df['V3(V)']#[0:1000]
 
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot(x, y, z, label='parametric curve')
 ax.legend()
-#ax.xlabel('$V_1$ in V')
-#ax.ylabel('$V_2$ in V')
-#ax.zlabel('$V_3$ in V')
-#ax.subplots_adjust(bottom=0.5)
+ax.set_xlabel('$V_1$ in V')
+ax.set_ylabel('$V_2$ in V')
+ax.set_zlabel('$V_3$ in V')
 
+ax.view_init(azim=145, elev=40)
 
 
 plt.show()
