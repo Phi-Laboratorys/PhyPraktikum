460bf3210cb9533a22dfe52e141bb2c1edea39f4
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-09-08 17:44:25 +0200

    PPB2 Chaos; Neues Package und plots

--- Versuch_Chaos/42-TeilauswertungPaul/42-1-AufgA.py
+++ Versuch_Chaos/42-TeilauswertungPaul/42-1-AufgA.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
+from matplotlib import rc
 from scipy.optimize import curve_fit
+
+rc('text', usetex=True)
+rc('font', family='serif')
 
 data = 'Versuch_Chaos/42-TeilauswertungPaul/AufgA.csv'
 df = pd.read_csv(data)
 
-def hyperbel(x,a,b,c):
+def hyperbel(x,a,b):
     #return b((x**2)/(a**2)**0.5)
-    return a/(c*x**3) + b
+    return a/(x**4) + b
 
 def fit(x,y):
     popt, _ = curve_fit(hyperbel, x, y)
@@ -16,17 +20,23 @@ def fit(x,y):
 
 
 #print(df)
+farben = ['red', 'green', 'blue','c','m', 'y', 'lime']
 for i in range(0,7):
     x = [df['x1'][i], df['x2'][i],df['x3'][i]]
     y = [df['y1'][i], df['y2'][i],df['y3'][i]]
     #print(x)
-    a, b, c = fit(x,y)
+    a, b = fit(x,y)
     x_line = np.linspace(10,100,100)
-    y_line = hyperbel(x_line,a,b,c)
-    plt.plot(x_line, y_line, label=df['Uebergang'][i])
-    plt.plot(x,y,'.')
+    y_line = hyperbel(x_line,a,b)
+    plt.plot(x_line, y_line, label=df['Uebergang'][i], color=farben[i], lw=0.5)
+    plt.plot(x,y,'x',color=farben[i])
 
+plt.plot([0,100], [8.4,8.4], ls='--', lw=0.5, color='gray', label='$R_2 = 8,4 \, k\Omega$')
+plt.xlabel('$R_1$ in $k\Omega$')
+plt.ylabel('$R_2$ in $k\Omega$')
+plt.xlim(10,100)
 plt.ylim(6,20)
+#plt.grid(True)
 plt.legend()
 plt.show()
 
@@ -34,4 +44,3 @@ plt.show()
 
 
 
-print(df)
