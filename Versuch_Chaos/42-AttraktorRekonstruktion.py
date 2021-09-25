4d2de98524e83fe022202ed32924ac9e4f8985fb
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-09-25 10:13:09 +0200

    Chaos AR py

--- Versuch_Chaos/42-AttraktorRekonstruktion.py
+++ Versuch_Chaos/42-AttraktorRekonstruktion.py
@@ -33,10 +33,13 @@ ax1.set_zlabel('$V_1(x+2n)$ in V')
 
 #Slider
 axstep = plt.axes([0.25,0.1,0.65,0.03], facecolor='r')
-step_slider = Slider(ax=axstep, label='n', valmin=1, valmax=200, valinit=60)
+step_slider = Slider(ax=axstep, label='n', valmin=1, valmax=200, valinit=60, valstep=1)
 
 def update(i):
-    
+    #ax1.set_xdata()
+    #ax1.set_ydata(df['V1(V)'].shift(periods=i))
+    ax1.set_3d_properties(df['V1(V)'].shift(periods=i*2))
+    plt.draw()
 
 step_slider.on_changed(update)
 plt.show()
