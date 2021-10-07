074c885e38d84e03c763485d0946b70d5421213f
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-10-07 16:44:57 +0200

    FRET Ausw Paul

--- Versuch_FRET/04-Versuchsauswertung/Paul/latextable.py
+++ Versuch_FRET/04-Versuchsauswertung/Paul/latextable.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 dataKfCFP = 'Versuch_FRET/04-Versuchsauswertung/Paul/KF-CFP.csv'
 dataKfYFP = 'Versuch_FRET/04-Versuchsauswertung/Paul/KF-YFP.csv'
+dataCY = 'Versuch_FRET/04-Versuchsauswertung/Paul/SeECY.csv'
 
-data = dataKfCFP
+data = dataCY
 
 df = pd.read_csv(data, decimal=',',sep=';')
 
 #print(df.head())
-print(df.to_latex(columns=['Zelle Nr.', 'D','S','beta'], index=False))
+print(df.to_latex(columns=['Zelle Nr.', 'D','S','A', 'SE', 'E'], index=False))
