89e19cb65e3df1a35ab9b87ca40c717540983eb3
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-10-08 18:58:32 +0200

    FRET

--- Versuch_FRET/04-Versuchsauswertung/Paul/bleach.py
+++ Versuch_FRET/04-Versuchsauswertung/Paul/bleach.py
@@ -8,6 +8,9 @@ from matplotlib import rc
 rc('text', usetex=True)
 rc('font', family='serif', size=20)
 
+def E(pre, post):
+    return 1 - pre/post
+
 # Data import
 path = 'Versuch_FRET/Daten/bleach-data/csv/'
 data_files = os.listdir(path)
@@ -15,16 +18,32 @@ data_files.sort()
 data_files.remove('.DS_Store')
 #print(data_files)
 
-df = pd.DataFrame(columns=['Name', ''])
+df = pd.DataFrame(columns=['Datenreihe', 'ROI 1', 'ROI 2', 'ROI 3'])
 
-string = 'S'
+string1 = 'S'
+string2 = 'CY'
+string3 = 'CFP'
 
 for d in data_files:
     
-    if d.find(string) == -1:
-        print(d)
-        #data = [path + '/' + i for i in data_files]
-        #df = pd.read_csv(d, skiprows=1)
+    if ((d.find(string1) == -1) and (d.find(string2) == -1) and (d.find(string3) == -1)):
+        #print(d)
+        data = path + '/' + d
+        dfd = pd.read_csv(data, skiprows=1)
+        
+        #Pre
+        dfpre = dfd.iloc[0:13]
+
+        #Post
+        dfpost = dfd.iloc[14:]
+        #print(dfpre)
+
+        E1 = E(dfpre['ROI1 []'].mean(), dfpost['ROI1 []'].mean())
+        E2 = E(dfpre['ROI2 []'].mean(), dfpost['ROI2 []'].mean())
+        E3 = E(dfpre['ROI3 []'].mean(), dfpost['ROI3 []'].mean())
+
+        dfa = pd.DataFrame([[d, E1, E2, E3]], columns=['Datenreihe', 'ROI 1', 'ROI 2', 'ROI 3'])
+        df = df.append(dfa, ignore_index=True)
 
     
-    
+print(df.to_latex(index=False))
