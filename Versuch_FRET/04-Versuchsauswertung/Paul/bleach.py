da6dcbbf357235b4c0f4ab4f9c58dd589af503dc
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-10-12 12:28:30 +0200

    FRET Verbesserung Chenyu's Nr. 2

--- Versuch_FRET/04-Versuchsauswertung/Paul/bleach.py
+++ Versuch_FRET/04-Versuchsauswertung/Paul/bleach.py
@@ -11,6 +11,9 @@ rc('font', family='serif', size=20)
 def E(pre, post):
     return 1 - pre/post
 
+def B(pre, post):
+    return post/pre
+
 # Data import
 path = 'Versuch_FRET/Daten/bleach-data/csv/'
 data_files = os.listdir(path)
@@ -20,7 +23,7 @@ data_files.remove('.DS_Store')
 
 df = pd.DataFrame(columns=['Datenreihe', 'ROI 1', 'ROI 2', 'ROI 3'])
 
-string1 = 'YFP'
+string1 = 'CFP'
 string2 = 'CY'
 string3 = 'S'
 
@@ -38,9 +41,9 @@ for d in data_files:
         dfpost = dfd.iloc[14:]
         #print(dfpre)
 
-        E1 = E(dfpre['ROI1 []'].mean(), dfpost['ROI1 []'].mean())
-        E2 = E(dfpre['ROI2 []'].mean(), dfpost['ROI2 []'].mean())
-        E3 = E(dfpre['ROI3 []'].mean(), dfpost['ROI3 []'].mean())
+        E1 = B(dfpre['ROI1 []'].mean(), dfpost['ROI1 []'].mean())
+        E2 = B(dfpre['ROI2 []'].mean(), dfpost['ROI2 []'].mean())
+        E3 = B(dfpre['ROI3 []'].mean(), dfpost['ROI3 []'].mean())
 
         dfa = pd.DataFrame([[d, E1, E2, E3]], columns=['Datenreihe', 'ROI 1', 'ROI 2', 'ROI 3'])
         df = df.append(dfa, ignore_index=True)
