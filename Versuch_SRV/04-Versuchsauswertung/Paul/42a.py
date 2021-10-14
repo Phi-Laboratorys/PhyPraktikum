e4bc0e53d99d7125b86a0b391554ea0d3a7eadbc
PaulSchwanitz <73427926+PaulSchwanitz@users.noreply.github.com>
2021-10-14 16:37:12 +0200

    SMR Korrektur Plots Paul

--- Versuch_SRV/04-Versuchsauswertung/Paul/42a.py
+++ Versuch_SRV/04-Versuchsauswertung/Paul/42a.py
@@ -10,15 +10,15 @@ rc('font', family='serif', size=15)
 
 data1 = 'Versuch_SRV/04-Versuchsauswertung/Paul/42a1.csv'
 data2 = 'Versuch_SRV/04-Versuchsauswertung/Paul/42a2.csv'
-data = data1
+data = data2
 df = pd.read_csv(data)
 
 #print(df.head())
 plt.figure(figsize=(12, 6), dpi=80)
-plt.plot(df['$f_{Abtast}$ in kHz'], df['$f_{Mes} in kHz$'], 'k.', label='Messpunkte')
-plt.plot([40,40], [0,21], linestyle='--', color='gray', label='r$f_\mathrm{crit}$')
+plt.plot(df['$f_{Quelle}$ in kHz'], df['$f_{Mes} in kHz$'], 'k.', label='Messpunkte')
+plt.plot([10,10], [0,11], linestyle='--', color='gray', label='r$f_\mathrm{crit}$')
 plt.xlabel(r'$f_\mathrm{Abtast}$ in kHz')
 plt.ylabel(r'$f_\mathrm{Mess}$ in kHz')
 plt.legend()
-plt.savefig('Versuch_SRV/Bilder/Paul/42a1.pdf', bbox_inches = 'tight')
+plt.savefig('Versuch_SRV/Bilder/Paul/42a2.pdf', bbox_inches = 'tight')
 plt.show()
