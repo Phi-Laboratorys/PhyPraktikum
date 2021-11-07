0c6ab26f94d997d754bad74e446f7bd60a3ffe10
ManeLippert <manuel-lippert@outlook.de>
2021-11-07 21:43:20 +0100

    SRV: Verbesserung 4.1

--- Versuch_SRV/04-Versuchsauswertung/Manuel/41.py
+++ Versuch_SRV/04-Versuchsauswertung/Manuel/41.py
@@ -31,7 +31,9 @@ data.sort()
 #data_sqare = [i for i in data if 'Rechteck' in i]
 #data_tri  =  [i for i in data if 'Dreieck' in i]
 
-plt.figure(figsize=(12,8), dpi=80)
+fig = plt.figure(figsize=(12,19), dpi=80)
+gs = fig.add_gridspec(3, hspace=0, height_ratios= [2,2,1])
+ax = gs.subplots(sharex=True)
 
 for i in data:
     df = pd.read_csv(i, skiprows=4, delim_whitespace= True)
@@ -42,9 +44,10 @@ for i in data:
     
     x = df['Fqscale-FFT']
     y, y_max = df['y-FFTcurve'], df['max/dB']
+    y_V = 1 * 10**(y/20) * np.sqrt(2) 
     x_t, y_g = df['time'], df['y-generator']
     
-    df_max = df[['Fqscale-FFT', 'max/dB']].dropna()
+    df_max = df[['Fqscale-FFT', 'y-FFTcurve', 'max/dB']].dropna()
     df_max = df_max[:][:-1]
     #df_max['max/dB'] = df_max['max/dB'] + 120
     
@@ -66,7 +69,7 @@ for i in data:
         df_max['diff/V'] = abs(-df_max['Theo/V'] + df_max['max/V'])
         df_max.index = np.arange(1, len(df_max) + 1)
             
-        print(df_max[0:35].to_latex())
+        #print(df_max[0:35].to_latex())
         
     if 'Rechteck' in i:
         print('Rechteck')
@@ -81,10 +84,13 @@ for i in data:
         
         df_max['diff/V'] = abs(-df_max['Theo/V'] + df_max['max/V'])
         df_max.index = np.arange(1, len(df_max) + 1)
+        
+        theo_dB = 20* np.log10(df_max['Theo/V']/np.sqrt(2))
             
-        print(df_max[0:35].to_latex())
-    
-        plt.plot(df_max['Fqscale-FFT'][0:35]/1000,df_max['diff/V'][0:35]*1E6,'o',label='Rechteck')
+        print(df_max.to_latex())
+        ax[0].plot(x/1000,y,color = '#ff7f0e', label='Rechteck')
+        ax[0].plot(df_max['Fqscale-FFT']/1000,theo_dB,'o',color = 'red',  label = 'Theorie')
+        ax[2].plot(df_max['Fqscale-FFT']/1000,df_max['diff/V']*1E6,'o',label='Rechteck', color='#ff7f0e')
         
     if 'Dreieck' in i:
         print('Dreieck')
@@ -99,16 +105,53 @@ for i in data:
         
         df_max['diff/V'] = abs(-df_max['Theo/V'] + df_max['max/V'])
         df_max.index = np.arange(1, len(df_max) + 1)
+        
+        theo_dB = 20* np.log10(df_max['Theo/V']/np.sqrt(2))
             
-        print(df_max[0:35].to_latex())
-    
-        plt.plot(df_max['Fqscale-FFT'][0:35]/1000,df_max['diff/V'][0:35]*1E6,'o',label='Dreieck')
-    
-plt.ylabel(r'$\Delta A$ in $\mu$V')
-plt.xlabel(r'$f$ in kHz')        
-plt.legend()
-plt.savefig('Versuch_SRV/Bilder/Manuel/41/Residuum.pdf',bbox_inches='tight')
-plt.show()
+        print(df_max.to_latex())
+        ax[1].plot(x/1000,y,color = '#1f77b4', label='Dreieck')
+        ax[1].plot(df_max['Fqscale-FFT']/1000,theo_dB,'o',color = 'red', label = 'Theorie')
+        ax[2].plot(df_max['Fqscale-FFT']/1000,df_max['diff/V']*1E6,'o',label='Dreieck', color='#1f77b4')
+
+ax[0].tick_params(direction = "in")
+
+axT = ax[0].secondary_xaxis('top')
+axT.tick_params(direction = "in")
+axT.xaxis.set_ticklabels([])
+
+axR = ax[0].secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+ax[1].tick_params(direction = "in")
+
+axT = ax[1].secondary_xaxis('top')
+axT.tick_params(direction = "in")
+axT.xaxis.set_ticklabels([])
+
+axR = ax[1].secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+ax[2].tick_params(direction = "in")
+
+axT = ax[2].secondary_xaxis('top')
+axT.tick_params(direction = "in")
+axT.xaxis.set_ticklabels([])
+
+axR = ax[2].secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+ax[0].set_ylabel(r'$A$ in dB')  
+ax[1].set_ylabel(r'$A$ in dB')  
+ax[2].set_ylabel(r'$\Delta A$ in $\mu$V')
+ax[2].set_xlabel(r'$f$ in kHz')  
+ax[0].legend(loc = 'lower right')
+ax[1].legend(loc = 'lower right')      
+ax[2].legend(loc='center right')
+#plt.savefig('Versuch_SRV/Bilder/Manuel/41/Residuum.pdf',bbox_inches='tight')
+#plt.show()
 '''
 '''
 #############################
@@ -126,13 +169,26 @@ df = pd.read_csv(data[1], skiprows=4, delim_whitespace= True)
 #df = df[:][:4500]
 df = df[:][:700]
 
-print(df)
+#print(df)
 
 x, y = df['Fqscale-FFT']/1000, df['y-FFTcurve']
-plt.figure(figsize=(12,8), dpi=80)
-plt.plot(x,y, color = 'black')
-plt.ylabel(r'$A$ in dB')
-plt.xlabel(r'$f$ in kHz')
+fig, ax = plt.subplots(figsize=(12,8), dpi=80)
+ax.plot(x,y, color = 'black')
+
+ax.tick_params(direction = "in")
+ax.set_ylabel(r'$U$ in V')
+ax.set_xlabel(r'$f$ in kHz')
+
+axT = ax.secondary_xaxis('top')
+axT.tick_params(direction = "in")
+axT.xaxis.set_ticklabels([])
+
+axR = ax.secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+ax.set_ylabel(r'$A$ in dB')
+ax.set_xlabel(r'$f$ in kHz')
 plt.savefig('Versuch_SRV/Bilder/Manuel/41/FourierSinus.pdf', bbox_inches='tight')
 plt.show()
 '''
@@ -148,27 +204,101 @@ data_files = os.listdir(path)
 data = [path + '/' + i for i in data_files if 'dat' in i]
 data.sort()
 
-n = [10, 100, 50, 0]
-colors = ['blue', 'orange', 'red', 'black']
-#print(data)
+data = [data[3],data[0],data[2],data[1]]
 
-plt.figure(figsize=(12,8), dpi=80)
-for i, n, c in zip(data, n, colors):
+N = [0, 10, 50, 100]
+colors = ['blue', 'orange', 'red', 'black']
+
+median = []
+
+fig, ax1 = plt.subplots(figsize=(12,8), dpi=80)
+for i, N, c in zip(data, N, colors):
     df = pd.read_csv(i, skiprows=4, delim_whitespace= True)
     #df = df[:][:4500]
     df = df[:][:300]
     
+    n = 50
+    df['max/dB'] = df.iloc[argrelextrema(df['y-FFTcurve'].values, np.greater_equal,order=n)[0]]['y-FFTcurve']
+    
     x, y = df['Fqscale-FFT']/1000, df['y-FFTcurve']
     
-    plt.plot(x,y, label = '$N$= '+str(n), color = c)
+    medi = np.repeat(y.median(),len(y))
+    maxi = df['max/dB']
     
-plt.ylabel(r'$A$ in dB')
-plt.xlabel(r'$f$ in kHz')
-plt.legend()
+    median.append(y.median())
+    
+    ax1.plot(x,y, label = '$N$= '+str(N), color = c)
+    ax1.plot(x,medi, linestyle = '--', color = c)
+
+ax1.plot(x,maxi, 'o', color = 'g', label='Maxima')
+    
+ax1.tick_params(direction = "in")
+
+ax1T = ax1.secondary_xaxis('top')
+ax1T.tick_params(direction = "in")
+ax1T.xaxis.set_ticklabels([])
+
+ax1R = ax1.secondary_yaxis('right')
+ax1R.tick_params(direction = "in")
+ax1R.yaxis.set_ticklabels([])    
+
+ax1.set_ylabel(r'$A$ in dB')
+ax1.set_xlabel(r'$f$ in kHz')
+ax1.legend()
 plt.savefig('Versuch_SRV/Bilder/Manuel/41/Mittelung.pdf', bbox_inches='tight')
 plt.show()
+
+fig, ax2 = plt.subplots(figsize=(12,8), dpi=80)
+
+maxima = maxi.dropna()
+N = [0,10,50,100]
+
+abstand = abs(median - maxima[36])
+
+#abstand = []
+#
+#for i in maxima:
+#    abstand[:] = [abs(j - i) for j in median]
+#    print(abstand)
+
+def ln(x,a,t):
+    return a * np.log(x+1) + t
+
+def sqrt(x,a,t):
+    return a * np.sqrt(x) + t
+
+x_lin = np.linspace(0,100, 1000)
+
+popt1, _ = curve_fit(ln, N, abstand)
+a1, t1 = popt1
+
+popt2, _ = curve_fit(sqrt, N, abstand)
+a2, t2 = popt2
+
+print(popt1, popt2)
+
+ax2.plot(N,abstand, 'o',color = 'black', label = 'Messreihe')
+ax2.plot(x_lin, ln(x_lin,a1,t1), label = 'Logaritmus-Fit')
+ax2.plot(x_lin, sqrt(x_lin,a2,t2), label = 'Wurzel-Fit')
+
+ax2.tick_params(direction = "in")
+
+ax2T = ax2.secondary_xaxis('top')
+ax2T.tick_params(direction = "in")
+ax2T.xaxis.set_ticklabels([])
+
+ax2R = ax2.secondary_yaxis('right')
+ax2R.tick_params(direction = "in")
+ax2R.yaxis.set_ticklabels([])    
+
+ax2.set_xlabel(r'$N$')
+ax2.set_ylabel(r'$d$ in dB')
+ax2.legend()
+
+plt.savefig('Versuch_SRV/Bilder/Manuel/41/MittlungAbstand.pdf', bbox_inches='tight')
+plt.show()
 '''
-#'''
+'''
 #############################
 ##                         ##
 ##      Teilaufgabe d      ##
@@ -182,31 +312,42 @@ data.sort()
 
 data = [data[2],data[1],data[3],data[0]]
 
-n = ['10\%', '50\%', '100\%', '120\%']
+n = ['10\,\%', '50\,\%', '100\,\%', '120\,\%']
 colors = ['red', 'orange', 'black', 'blue']
 
-fig, ax1 = plt.subplots(figsize=(12,8), dpi=80)
+fig, ax = plt.subplots(figsize=(12,8), dpi=80)
 
 #rect = Rectangle((0.17, 0.22), 0.65, 0.47, facecolor='white', ec='none', alpha=1, transform=fig.transFigure, zorder=-1)
 
-axins = inset_axes(ax1, 6,3 , loc='center') # insert pic
-mark_inset(ax1,axins,loc1=2,loc2=1)
+axins = inset_axes(ax, 6,3 , loc='center') # insert pic
+mark_inset(ax,axins,loc1=2,loc2=1)
+
+y_winmin, y_winmax = 0.5, 0.5
 
 for i, n, c in zip(data, n, colors):
     df = pd.read_csv(i, skiprows=4, delim_whitespace= True)
     #df = df[:][:4500]
     df = df[:][:251]
+    #df = df[:][2:125]
+
     df_win = df[:][2:125]
     
     #x, y = df['Fqscale-FFT']/1000, df['y-FFTcurve']
     x, y = df['time'], df['y-generator']
     x_win, y_win = df_win['time'], df_win['y-generator']
     
-    ax1.plot(x,y, label = '$d$ = '+n, color = c)
-    axins.plot(x,y, label = '$d$ = '+n, color = c)
+    if y_win.min() < y_winmin:
+        y_winmin =  y_win.min()
+        
+    if y_win.max() > y_winmax:
+        y_winmax = y_win.max()
+    
+    ax.plot(x,y, label = '$V_{\mathrm{SR}}$ = '+n, color = c)
+    axins.plot(x,y, label = '$V_{\mathrm{SR}}$ = '+n, color = c)
 
 axins.set_xlim(x_win.min(),x_win.max())
-axins.set_ylim(y_win.min(),y_win.max())
+axins.set_ylim(y_winmin,y_winmax)
+axins.tick_params(direction = "in")
 
 #axins.patch.set_facecolor('white')
 #axins.patch.set_alpha(0.5)
@@ -214,21 +355,36 @@ axins.set_ylim(y_win.min(),y_win.max())
 
 #axins.set_ylabel(r'$U$ in V')
 #axins.set_xlabel(r'$f$ in kHz')
-ax1.tick_params(direction = "in")
-axT = ax1.secondary_xaxis('top')
+
+axinsT = axins.secondary_xaxis('top')
+axinsT.tick_params(direction = "in")
+axinsT.xaxis.set_ticklabels([])
+
+axinsR = axins.secondary_yaxis('right')
+axinsR.tick_params(direction = "in")
+axinsR.yaxis.set_ticklabels([])
+
+ax.tick_params(direction = "in")
+ax.set_ylabel(r'$U$ in V')
+ax.set_xlabel(r'$f$ in kHz')
+
+axT = ax.secondary_xaxis('top')
 axT.tick_params(direction = "in")
 axT.xaxis.set_ticklabels([])
-ax1.set_ylabel(r'$U$ in V')
-ax1.set_xlabel(r'$f$ in kHz')
 
-axins.legend(loc = (0.87,0.9)) 
+axR = ax.secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+axins.legend(loc = (0.81,0.88)) 
+#ax.legend()
 
 #plt.savefig('Versuch_SRV/Bilder/Manuel/41/SignalRauschAbstandKomplett.pdf', bbox_inches='tight')
 #plt.savefig('Versuch_SRV/Bilder/Manuel/41/SignalRauschAbstand.pdf', bbox_inches='tight')
 plt.savefig('Versuch_SRV/Bilder/Manuel/41/SignalRauschAbstandAll.pdf', bbox_inches='tight')
 plt.show()
-#'''
 '''
+#'''
 #############################
 ##                         ##
 ##      Teilaufgabe e      ##
@@ -240,26 +396,81 @@ data_files = os.listdir(path)
 data = [path + '/' + i for i in data_files if 'dat' in i]
 data.sort()
 
-n = ['20MHz', '5MHz', '10MHz', '100kHz', '10kHz', '1kHz']
-colors = ['blue', 'orange', 'red', 'grey', 'purple', 'black']
-#print(data)
+data = [data[5],data[4],data[3],data[1],data[2],data[0]]
 
-plt.figure(figsize=(12,8), dpi=80)
+n = ['1\,kHz', '10\,kHz', '100\,kHz', '5\,MHz', '10\,MHz', '20\,MHz']
+colors = ['black', 'purple', 'grey', 'orange', 'red', 'blue']
+
+fig, ax = plt.subplots(figsize=(12,8), dpi=80)
+
+#rect = Rectangle((0.17, 0.11), 0.48, 0.44, facecolor='white', ec='none', alpha=1, transform=fig.transFigure, zorder=-1)
+
+#axins = inset_axes(ax, 5, 3 , loc='center', bbox_to_anchor=(0.4,0.34),  bbox_transform=ax.transAxes) # insert pic
+#mark_inset(ax,axins,loc1=2,loc2=1)
+
+y_winmin, y_winmax = 0.5, 0.5
+
 for i, n, c in zip(data, n, colors):
     df = pd.read_csv(i, skiprows=4, delim_whitespace= True)
     #df = df[:][:4500]
     df = df[:][:252]
-    #df = df[:][5:125]
+    #df = df[:][2:125]
+    df_win = df[:][2:125]
     
-    #x, y = df['Fqscale-FFT']/1000, df['y-FFTcurve']
+    x_F, y_F = df['Fqscale-FFT']/1000, df['y-FFTcurve']
     x, y = df['time'], df['y-generator']
+    x_win, y_win = df_win['time'], df_win['y-generator']
     
-    plt.plot(x,y, label = r'$f_\mathrm{Band}$ = '+n, color = c)
+    if y_win.min() < y_winmin:
+        y_winmin =  y_win.min()
+        
+    if y_win.max() > y_winmax:
+        y_winmax = y_win.max()
     
-plt.ylabel(r'$U$ in V')
-plt.xlabel(r'$f$ in kHz')
-plt.legend()
-plt.savefig('Versuch_SRV/Bilder/Manuel/41/BandbreiteKomplett.pdf', bbox_inches='tight')
+    ax.plot(x_F,y_F, label = r'$f_\mathrm{Band}$ = '+n, color = c)
+    #ax.plot(x,y, label = r'$f_\mathrm{Band}$ = '+n, color = c)
+    #axins.plot(x,y, label = r'$f_\mathrm{Band}$ = '+n, color = c)
+    
+#axins.set_xlim(x_win.min(),x_win.max())
+#axins.set_ylim(y_winmin,y_winmax)
+#axins.tick_params(direction = "in")
+
+#axins.patch.set_facecolor('white')
+#axins.patch.set_alpha(0.5)
+#axins.patches.append(rect)
+
+#axinsT = axins.secondary_xaxis('top')
+#axinsT.tick_params(direction = "in")
+#axinsT.xaxis.set_ticklabels([])
+
+#axinsR = axins.secondary_yaxis('right')
+#axinsR.tick_params(direction = "in")
+#axinsR.yaxis.set_ticklabels([])
+
+#axins.set_ylabel(r'$U$ in V')
+#axins.set_xlabel(r'$f$ in kHz')
+
+ax.tick_params(direction = "in")
+ax.set_ylabel(r'$U$ in V')
+ax.set_xlabel(r'$f$ in kHz')
+
+ax.set_ylabel(r'$A$ in dB')
+ax.set_xlabel(r'$f$ in kHz')
+
+axT = ax.secondary_xaxis('top')
+axT.tick_params(direction = "in")
+axT.xaxis.set_ticklabels([])
+
+axR = ax.secondary_yaxis('right')
+axR.tick_params(direction = "in")
+axR.yaxis.set_ticklabels([])
+
+#axins.legend(loc = (0.97,0.93)) 
+ax.legend()
+    
+#plt.savefig('Versuch_SRV/Bilder/Manuel/41/BandbreiteKomplett.pdf', bbox_inches='tight')
 #plt.savefig('Versuch_SRV/Bilder/Manuel/41/Bandbreite.pdf', bbox_inches='tight')
+#plt.savefig('Versuch_SRV/Bilder/Manuel/41/BandbreiteAll.pdf', bbox_inches='tight')
+plt.savefig('Versuch_SRV/Bilder/Manuel/41/BandbreiteFourier.pdf', bbox_inches='tight')
 plt.show()
-'''
+#'''
